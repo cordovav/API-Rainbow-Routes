1839e71decd146849db0d8d4570c248807c5fa68
Veny Cordova <venycordova@gmail.com>
2023-04-15 14:21:29 -0400

    practicing ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 // Require needed node modules
-const express = require('express')
+import express from 'express'
 
 // Initialize your application by calling the function returned by the express module
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 // Declare routes that people can visit on the application
 
 // White Page
-app.get('/', function (req, res) {
+app.get('/', function (req: express.Request, res: express.Response):void {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
@@ -21,10 +21,12 @@ app.get('/', function (req, res) {
     `)
 })
 
-
 // Color Page
+//we use narrowing to narrow the type to a string
+//also we us unknown if we dont know what type of data it is goin to be 
 app.get('/:color', function (req, res) {
-    let myColor = req.params.color
+    let myColor: unknown = req.params.color
+    if (typeof(myColor) === 'string'){
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
@@ -33,6 +35,7 @@ app.get('/:color', function (req, res) {
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
     `)
+    }
 })
 
 
